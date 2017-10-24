05b850ff5b98be4c9322ab1fb4e43ac8920f4603
TQ24 <yquan20@colby.edu>
2017-10-23 23:56:03 -0400

    cpp

--- Project5/C++/Task1.cpp
+++ Project5/C++/Task1.cpp
@@ -11,66 +11,186 @@
 #include <iostream>
 using namespace std;
 
-typedef struct Node {
-  sturct Node *next;
-  void *data;
-} Node;
+struct node
+{
+  node *next;
+  float data;
+};
 
-class LinkedList {
-  Private:
-  Node *head;
-  Node *tail;
+class LinkedList
+{
+private:
+  node *head;
+  node *tail;
   int size;
-
-  Public:
+public:
   // constructor
-  LinkedList(){
-    this.head = NULL;
-    this.tail = NULL;
-    this.size = 0;
+  LinkedList()
+  {
+    head = NULL;
+    tail = NULL;
+    size = 0;
+  }
+
+  node *gethead(){
+    return head;
   }
 
   // adds a node to the front of the list, storing the given data in the node.
-  void ll_push( void *data ){
-    Node newnode = new Node();
+  void ll_push( float data ){
+    node *newnode = new node;
     newnode->data = data;
-    if ( this.size == 0 ){
-      this.head = newnode;
-      this.tail = newnode;
-      this.size++;
+    if ( size == 0 ){
+      head = newnode;
+      tail = newnode;
+      newnode->next = NULL;
+      size++;
     }
     else{
-      Node *oldhead = this.head;
-      this.head = newnode;
+      node *oldhead = head;
+      head = newnode;
       newnode->next = oldhead;
-      this.size++;
+      size++;
     }
   }
 
   // removes the node at the front of the list and returns the associate data.
-  void *ll_pop(){
-    if (this.head == NULL){
-      return NULL;
+  void ll_pop(){
+    if (head == NULL){
+
     }
-    else if (this.size == 1){
-      Node *pop = this.head;
-      this.head = NULL;
-      this.tail = NULL;
-      this.size--;
-      return pop->data;
+    else if (size == 1){
+      node *pop = head;
+      head = NULL;
+      tail = NULL;
+      size--;
+
     }
-    Node *pop = this.head;
-    this.head = pop->next;
-    this.size--;
-    return pop->data;
+    node *pop = head;
+    head = pop->next;
+    size--;
   }
 
-  int main(){
-    LinkedList ll = new LinkedList();
-    int *a = 24;
-    ll.ll_push(a);
-    std::cout<< ll.head->data <<;
+  // adds a node to the end of the list, storing the given data in the node
+  void ll_append( float data ){
+    node *newnode = new node;
+    newnode->data = data;
+    if (size==0){
+      head = newnode;
+      tail = newnode;
+      size++;
+    }
+    else {
+      node *curnode = head;
+      while (curnode->next!=NULL){
+        curnode = curnode->next;
+      }
+      curnode->next = newnode;
+      newnode->next = NULL;
+      size++;
+    }
   }
 
+  // removes the first node in the list whos data matches target, freeing the associated data.
+  void ll_remove(float target){
+    if (size == 0){
 
+    }
+    else if (size == 1){
+      node *curhead = head;
+      if (curhead->data==target){
+        delete curhead;
+        head = NULL;
+        tail = NULL;
+        size--;
+      }
+      else{}
+    }
+    else{
+      node *curhead = head;
+      if (curhead->data == target){
+        head = curhead->next;
+        delete curhead;
+        size--;
+      }
+      else{
+        node *last = curhead;
+        curhead = curhead->next;
+        while (curhead!=NULL){
+          if (curhead->data == target){
+            last->next = curhead->next;
+            delete curhead;
+            size--;
+          }
+          else {
+            curhead = curhead->next;
+            last = last->next;
+          }
+        }
+      }
+    }
+  }
+
+  // returns the size of the list.
+  int ll_size(){
+    return size;
+  }
+
+  // removes all of the nodes from the list
+  void ll_clear(){
+    node *curhead = head;
+    if (curhead == NULL){}
+    else{
+      while (curhead!=NULL){
+        node *clear = curhead;
+        curhead = curhead->next;
+        delete clear;
+      }
+      head = NULL;
+      tail = NULL;
+      size = 0;
+    }
+  }
+
+  // traverses the list and applies the give function to the data at each node.
+  void ll_map(void (*mapfunc)(float)){
+    node *cur = head;
+    if (cur==NULL){
+
+    }
+    else{
+      while (cur!=NULL){
+        mapfunc(cur->data);
+        cur = cur->next;
+      }
+    }
+  }
+
+  void ll_display(){
+    node *cur = head;
+    if (cur == NULL){
+      std::cout<< "This list is empty!\n";
+    }
+    else {
+      std::cout<< "Linked List: ";
+      while (cur!=NULL){
+        std::cout<< cur->data << " ";
+        cur = cur->next;
+      }
+    }
+    std::cout<< "\n";
+  }
+};
+
+void square(float i){
+  i = i*i;
+}
+
+int main(){
+  LinkedList *ll = new LinkedList;
+  int a = 24;
+  ll->ll_push(a);
+  //std::cout<< ll->gethead()->data << " ";
+  ll->ll_display();
+  return 0;
 }
